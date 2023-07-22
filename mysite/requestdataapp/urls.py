0cd666f0df790ecfb9b85a84af5d772023c56aa6
Sergey Lebedev <sergeylebedev87@gmail.com>
2023-07-22 13:15:13 +0300

    Homework 6

--- mysite/requestdataapp/urls.py
+++ mysite/requestdataapp/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from .views import process_get_view, file_download
+from .views import process_get_view, file_download, user_form, handle_file_upload
 
 app_name = 'requestdataapp'
 
 urlpatterns = [
     path('get/', process_get_view, name='get-view'),
-    path('file/', file_download, name='file-upload')
+    path('file/', file_download, name='file-upload'),
+    path("bio/", user_form, name="user-form"),
+    path("upload/", handle_file_upload, name="file-upload-form"),
 ]
