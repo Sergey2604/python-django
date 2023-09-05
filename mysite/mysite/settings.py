bdf5cf419891a8d58f10247e5c7bae08209c2d63
Sergey Lebedev <sergeylebedev87@gmail.com>
2023-09-06 01:47:34 +0300

    Homework 20-1

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+from os import getenv
+import logging.config
 from django.urls import reverse_lazy
 from django.utils.translation import gettext_lazy as _
 
@@ -30,19 +31,22 @@ sentry_sdk.init(
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+DATABASE_DIR = BASE_DIR / "database"
+DATABASE_DIR.mkdir(exist_ok = True)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a3()##e4dlss10143l83d9ejch*kzuzm7d1@duj8_840g_6m_#'
+SECRET_KEY = getenv('DJANGO_SECRET_KEY',
+                    'django-insecure-a3()##e4dlss10143l83d9ejch*kzuzm7d1@duj8_840g_6m_#')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = getenv('DJANGO_DEBUG', 0) == 1
 
 ALLOWED_HOSTS = [
-    "0.0.0.0",
-    "127.0.0.1",
-]
+                    "0.0.0.0",
+                    "127.0.0.1",
+                ] + getenv('DJANGO_ALLOWED_HOSTS', '').split(',')
 INTERNAL_IPS = ['127.0.0.1', "0.0.0.0"]
 
 if DEBUG:
@@ -122,7 +126,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': DATABASE_DIR / 'db.sqlite3',
     }
 }
 
@@ -209,31 +213,26 @@ SPECTACULAR_SETTINGS = {
 LOGFILE_NAME = BASE_DIR / "log.log"
 LOGFILE_SIZE = 400
 LOGFILE_COUNT = 3
-
-# LOGGING = {
-#     'version': 1,
-#     'disable_existing_loggers': False,
-#     'formatters': {
-#         'verbose': {
-#             'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
-#         }
-#     },
-#     'handlers': {
-#         'console': {
-#             'class': 'logging.StreamHandler',
-#             'formatter': 'verbose',
-#         },
-#         'logfile': {
-#             'class': 'logging.handlers.RotatingFileHandler',
-#             'filename': LOGFILE_NAME,
-#             'maxBytes': LOGFILE_SIZE,
-#             'backupCount': LOGFILE_COUNT,
-#             'formatter': 'verbose',
-#         }
-#     },
-#     'root': {
-#         'handlers': ['console', 'logfile'],
-#         'level': 'DEBUG'
-#     },
-#
-# }
+LOGLEVEL = getenv('DJANGO-LOGLEVEL', 'info').upper()
+logging.config.dictConfig({
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'console': {
+            'format': '%(asctime)s (levelname)s [(name)s (lineno)s] ('
+                      'module)s (message)s'
+        },
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'console',
+        },
+    },
+    'loggers': {
+        '': {
+            'level': LOGLEVEL,
+            'handlers': ['console', ],
+        },
+    },
+})
